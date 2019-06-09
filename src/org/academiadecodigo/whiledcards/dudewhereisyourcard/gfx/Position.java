ec323c4bb53267d4a2ff77272ab584e0aa211916
rafaelmdurante <rafaelmdurante@gmail.com>
2019-06-09 16:22:32 +0100

    class position created

--- src/org/academiadecodigo/whiledcards/dudewhereisyourcard/gfx/Position.java
+++ src/org/academiadecodigo/whiledcards/dudewhereisyourcard/gfx/Position.java
@@ -1,4 +1,30 @@
 package org.academiadecodigo.whiledcards.dudewhereisyourcard.gfx;
 
+import org.academiadecodigo.simplegraphics.pictures.*;
+
 public class Position {
+    private int col;
+    private int row;
+    private Grid grid;
+    private Picture picture;
+
+    public Position(Grid grid) {
+        this.grid = grid;
+        col = (int) (Math.random() * grid.getWidth());
+        row = (int) (Math.random() * grid.getHeight());
+    }
+
+     public Position(int col, int row, Grid grid) {
+        this.grid = grid;
+        this.col = col;
+        this.row = row;
+    }
+
+    public void show() {
+        picture.draw();
+    }
+
+    public void hide() {
+        picture.delete();
+    }
 }
